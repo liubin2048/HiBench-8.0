3ae36160f159e25738786c5e62482f3024b263a1
LiuBin <liubin3@bonc.com.cn>
2018-12-28 15:47:45 +0800

    HiBench

---
+++ common/src/main/scala/com/intel/hibench/common/streaming/metrics/KafkaCollector.scala
@@ -0,0 +1,137 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.hibench.common.streaming.metrics
+
+import java.io.{File, FileWriter}
+import java.util.concurrent.{Executors, Future, TimeUnit}
+import java.util.{Date, Properties}
+
+import com.codahale.metrics.{Histogram, UniformReservoir}
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.PartitionInfo
+import org.apache.kafka.common.serialization.StringDeserializer
+
+import scala.collection.mutable.ArrayBuffer
+
+
+class KafkaCollector(bootstrapServers: String, metricsTopic: String,
+                     outputDir: String, sampleNumber: Int, desiredThreadNum: Int) extends LatencyCollector {
+
+  private val histogram = new Histogram(new UniformReservoir(sampleNumber))
+  private val threadPool = Executors.newFixedThreadPool(desiredThreadNum)
+  private val fetchResults = ArrayBuffer.empty[Future[FetchJobResult]]
+
+  def start(): Unit = {
+    val partitions = getPartitions(metricsTopic)
+
+    println("Starting MetricsReader for kafka topic: " + metricsTopic)
+
+    partitions.foreach(partition => {
+      val job = new FetchJob(bootstrapServers, metricsTopic, partition, histogram)
+      val fetchFeature = threadPool.submit(job)
+      fetchResults += fetchFeature
+    })
+
+    threadPool.shutdown()
+    threadPool.awaitTermination(30, TimeUnit.MINUTES)
+
+    val finalResults = fetchResults.map(_.get()).reduce((a, b) => {
+      val minTime = Math.min(a.minTime, b.minTime)
+      val maxTime = Math.max(a.maxTime, b.maxTime)
+      val count = a.count + b.count
+      new FetchJobResult(minTime, maxTime, count)
+    })
+
+    report(finalResults.minTime, finalResults.maxTime, finalResults.count)
+  }
+
+  //  private def getPartitions(topic: String, zkConnect: String): Seq[Int] = {
+  //    //    val zkClient = new ZkClient(zkConnect, 6000, 6000, SerializableSerializer)
+  //    val zkUtils = ZkUtils.apply(zkConnect, 6000, 6000, false)
+  //    try {
+  //      zkUtils.getPartitionsForTopics(Seq(topic)).flatMap(_._2).toSeq
+  //    } finally {
+  //      zkUtils.close()
+  //    }
+  //  }
+
+  private val CLIENT_ID = "metrics_reader"
+
+  private val props = new Properties()
+  props.put("key.deserializer", classOf[StringDeserializer])
+  props.put("value.deserializer", classOf[StringDeserializer])
+  props.put("group.id", CLIENT_ID)
+  props.put("bootstrap.servers", bootstrapServers)
+  props.put("enable.auto.commit", (false: java.lang.Boolean))
+  props.put("auto.offset.reset", "latest")
+
+  private def getPartitions(topic: String): Seq[Int] = {
+    import scala.collection.JavaConversions._
+    val consumer = new KafkaConsumer[String, String](props);
+    val partitionInfo = consumer.partitionsFor(topic)
+
+    val topicPartitions = Seq[Int]()
+    for (info: PartitionInfo <- partitionInfo) {
+      topicPartitions.addString(new StringBuilder(info.partition()))
+    }
+
+    topicPartitions
+  }
+
+
+  private def report(minTime: Long, maxTime: Long, count: Long): Unit = {
+    val outputFile = new File(outputDir, metricsTopic + ".csv")
+    println(s"written out metrics to ${outputFile.getCanonicalPath}")
+    val header = "time,count,throughput(msgs/s),max_latency(ms),mean_latency(ms),min_latency(ms)," +
+      "stddev_latency(ms),p50_latency(ms),p75_latency(ms),p95_latency(ms),p98_latency(ms)," +
+      "p99_latency(ms),p999_latency(ms)\n"
+    val fileExists = outputFile.exists()
+    if (!fileExists) {
+      val parent = outputFile.getParentFile
+      if (!parent.exists()) {
+        parent.mkdirs()
+      }
+      outputFile.createNewFile()
+    }
+    val outputFileWriter = new FileWriter(outputFile, true)
+    if (!fileExists) {
+      outputFileWriter.append(header)
+    }
+    val time = new Date(System.currentTimeMillis()).toString
+    val count = histogram.getCount
+    val snapshot = histogram.getSnapshot
+    val throughput = count * 1000 / (maxTime - minTime)
+    outputFileWriter.append(s"$time,$count,$throughput," +
+      s"${formatDouble(snapshot.getMax)}," +
+      s"${formatDouble(snapshot.getMean)}," +
+      s"${formatDouble(snapshot.getMin)}," +
+      s"${formatDouble(snapshot.getStdDev)}," +
+      s"${formatDouble(snapshot.getMedian)}," +
+      s"${formatDouble(snapshot.get75thPercentile())}," +
+      s"${formatDouble(snapshot.get95thPercentile())}," +
+      s"${formatDouble(snapshot.get98thPercentile())}," +
+      s"${formatDouble(snapshot.get99thPercentile())}," +
+      s"${formatDouble(snapshot.get999thPercentile())}\n")
+    outputFileWriter.close()
+  }
+
+  private def formatDouble(d: Double): String = {
+    "%.3f".format(d)
+  }
+
+}
---
+++ common/src/main/scala/com/intel/hibench/common/streaming/metrics/KafkaConsumerOwn.scala
@@ -0,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.hibench.common.streaming.metrics
+
+import java.util
+import java.util.{Collections, Properties}
+
+import kafka.api.{FetchRequestBuilder, OffsetRequest}
+import kafka.common.ErrorMapping._
+import kafka.common.TopicAndPartition
+import kafka.consumer.{ConsumerConfig, SimpleConsumer}
+import kafka.message.MessageAndOffset
+import kafka.utils.ZkUtils
+import org.apache.kafka.clients.admin.KafkaAdminClient
+import org.apache.kafka.clients.consumer.{ConsumerRecord, KafkaConsumer}
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.utils.Utils
+
+class KafkaConsumerOwn(bootstrapServers: String, topic: String, partition: Int) {
+
+  private val CLIENT_ID = "metrics_reader"
+
+  private val props = new Properties()
+  props.put("key.deserializer", classOf[StringDeserializer])
+  props.put("value.deserializer", classOf[StringDeserializer])
+  props.put("group.id", CLIENT_ID)
+  props.put("bootstrap.servers", bootstrapServers)
+  props.put("enable.auto.commit", (false: java.lang.Boolean))
+  props.put("auto.offset.reset", "latest")
+
+  private val consumer = createConsumer
+
+  val tp = new TopicPartition(topic, partition)
+  private val earliestOffset = consumer.beginningOffsets(util.Arrays.asList(tp)).get(tp)
+
+//    .earliestOrLatestOffset(TopicAndPartition(topic, partition), OffsetRequest.EarliestTime, -1)
+  private var nextOffset: Long = earliestOffset
+
+//  private var iterator: Iterator[MessageAndOffset] = getIterator(nextOffset)
+  private var iterator: util.Iterator[ConsumerRecord[String, String]] = getIterator(nextOffset)
+
+  def next(): Array[Byte] = {
+    val record = iterator.next()
+    nextOffset = record.offset()+1
+    record.value().getBytes
+  }
+
+//  def next(): Array[Byte] = {
+//    val mo = iterator.next()
+//    val message = mo.message
+//
+//    nextOffset = mo.nextOffset
+//
+//    Utils.readBytes(message.payload)
+//  }
+
+  def hasNext: Boolean = {
+        @annotation.tailrec
+        def hasNextHelper(iter: util.Iterator[ConsumerRecord[String, String]], newIterator: Boolean): Boolean = {
+          if (iter.hasNext) true
+          else if (newIterator) false
+          else {
+            iterator = getIterator(nextOffset)
+            hasNextHelper(iterator, newIterator = true)
+          }
+        }
+
+        hasNextHelper(iterator, newIterator = false)
+  }
+
+//  def hasNext: Boolean = {
+//    @annotation.tailrec
+//    def hasNextHelper(iter: Iterator[MessageAndOffset], newIterator: Boolean): Boolean = {
+//      if (iter.hasNext) true
+//      else if (newIterator) false
+//      else {
+//        iterator = getIterator(nextOffset)
+//        hasNextHelper(iterator, newIterator = true)
+//      }
+//    }
+//
+//    hasNextHelper(iterator, newIterator = false)
+//  }
+
+  def close(): Unit = {
+    consumer.close()
+  }
+
+  private def createConsumer: KafkaConsumer[String, String] = {
+    val kafkaComsumer = new KafkaConsumer[String, String](props)
+    kafkaComsumer.subscribe(Collections.singleton(topic))
+    kafkaComsumer
+  }
+
+  private def getIterator(offset: Long): util.Iterator[ConsumerRecord[String, String]] = {
+    consumer.seek(tp, offset)
+    val records = consumer.poll(6000)
+    records.iterator()
+  }
+
+//  private def getIterator(offset: Long): Iterator[MessageAndOffset] = {
+//
+//    val request = new FetchRequestBuilder()
+//      .addFetch(topic, partition, offset, config.fetchMessageMaxBytes)
+//      .build()
+//
+//    val response = consumer.fetch(request)
+//    response.errorCode(topic, partition) match {
+//      case NoError => response.messageSet(topic, partition).iterator
+//      case error => throw exceptionFor(error)
+//    }
+//  }
+}
